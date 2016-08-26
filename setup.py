107d9f95754176bfacdbc51d968f823d6457fb5c
Aleksander Drenik <adrenik@ipp.mpg.de>
2016-08-26 08:48:42 +0200

    Moved files to rga_data_handling directory, added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='RGA data handling',
+    version='b1.0',
+    packages=['rga_data_handling',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('readme.txt').read(),
+)
